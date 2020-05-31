a8a7e7eaf973fe060d62c0ca21050b2124e7b2e3
AgusNicoloso <agustinnicoloso@uxorit.com>
2020-05-31 19:48:20 -0300

    Updates

--- precache-manifest.56571eced995471ae9d9d0463522bc1f.js
+++ precache-manifest.56571eced995471ae9d9d0463522bc1f.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "628c2c9dccd97223975ae7d82caf0e19",
+    "revision": "6ba1caebca94a7c31ae5a02c85c20e09",
     "url": "/Routear/index.html"
   },
   {
